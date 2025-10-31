20d28a890ddbe400e2e5aa1ae0d61cc4ab202ba1
Christian Belloni <christian.belloni@babyloncloud.com>
2025-10-31 15:52:06 +0100

    .

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -34,25 +34,30 @@ where
 }
 
 /// Opens an HTTP/1.1 connection to a remote host.
-pub async fn connect_custom_buf_reader<RW, B, K, F1, F2>(
+pub async fn connect_custom_buf_reader<RW, B, K, M, F1, F2, F3>(
     mut stream: RW,
     req: Request,
     buf_read_fn_1: F1,
     buf_read_fn_2: F2,
+    buf_read_fn_3: F3,
 ) -> http_types::Result<Response>
 where
-    RW: Read + Write + Send + Sync + Unpin + 'static,
+    RW: AsyncBufRead + Write + Send + Sync + Unpin + 'static,
     B: AsyncBufRead + Send + Sync + 'static + Unpin,
-    F1: Fn(RW) -> B,
-    F2: Fn(ChunkedDecoder<B>) -> K,
+    F1: Fn(Encoder) -> M,
+    F2: Fn(RW) -> B,
+    F3: Fn(ChunkedDecoder<B>) -> K,
     K: Send + Sync + 'static + Unpin + AsyncBufRead,
+    M: AsyncBufRead + Send + Sync + 'static + Unpin,
 {
-    let mut req = Encoder::new(req);
+    let req = Encoder::new(req);
     log::trace!("> {:?}", &req);
 
-    io::copy(&mut req, &mut stream).await?;
+    let mut req = buf_read_fn_1(req);
 
-    let res = decode_custom_buf_read(stream, buf_read_fn_1, buf_read_fn_2).await?;
+    io::copy_buf(&mut req, &mut stream).await?;
+
+    let res = decode_custom_buf_read(stream, buf_read_fn_2, buf_read_fn_3).await?;
     log::trace!("< {:?}", &res);
 
     Ok(res)
